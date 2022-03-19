6ac176376829d513ed775c15e13386a9c954a5f1
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-03-19 11:57:23 +0000

    Add styles for desktop

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@
   }
 
   function handleSubmit() {
+    if (!rating) return;
     form.submitted = true;
   }
 </script>
@@ -15,7 +16,7 @@
   {#if !form.submitted}
     <div class="card">
       <div class="icon">
-        <img src="/icon-star.svg" alt="">
+        <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614"/></svg>
       </div>
 
       <h1>How did we do?</h1>
@@ -78,7 +79,7 @@
     width: 2.625rem;
     height: 2.625rem;
     border: 0;
-    border-radius: 2.625rem;
+    border-radius: 4rem;
     color: var(--medium-grey);
     background-color: var(--dark-blue);
     cursor: pointer;
@@ -98,16 +99,15 @@
   }
 
   .card {
-    background: linear-gradient(
-      to bottom,
-      hsl(213, 19%, 15%),
-      hsl(216, 12%, 12%)
+    background: radial-gradient(
+      circle at top,
+      hsl(215, 20%, 17%),
+      hsl(216, 18%, 12%)
     );
     padding: 1.5rem 1.5rem 2rem 1.5rem;
     border-radius: 1rem;
     max-width: 327px;
-    margin-left: auto;
-    margin-right: auto;
+    margin: 3px auto 0;
     min-height: 360px;
   }
 
@@ -171,6 +171,59 @@
 
   .mb-0 {
     margin-bottom: 0;
+  }
+
+  @media (min-width: 992px) {
+    h1 {
+      font-size: 1.75rem;
+      margin-bottom: 0.25rem;
+    }
+
+    p {
+      font-size: 1rem;
+      line-height: 1.5;
+    }
+
+    button {
+      font-size: 1rem;
+      line-height: 3.25rem;
+      width: 3.125rem;
+      height: 3.125rem;
+    }
+
+    .card {
+      max-width: 410px;
+      min-height: 415px;
+      border-radius: 2rem;
+      padding: 2rem 2.5rem 2rem 2rem;
+    }
+
+    .icon {
+      width: 3rem;
+      height: 3rem;
+      border-radius: 3rem;
+      margin-bottom: 1.75rem;
+    }
+
+    .btn-group {
+      margin-bottom: 2rem;
+    }
+
+    .btn-primary {
+      font-size: 1rem;
+    }
+
+    .illustration {
+      width: 162px;
+      margin: 0.875rem auto 2rem;
+    }
+
+    .badge {
+      font-size: 0.875rem;
+      margin-bottom: 2rem;
+      padding: 10px 1.25rem 7px;
+      letter-spacing: 0.01em;
+    }
   }
 
 </style>
